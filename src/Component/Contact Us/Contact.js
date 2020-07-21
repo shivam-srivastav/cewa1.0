faff604bb0ac5ece70d7e31a685729ecaafc83fd
shivam1534 <shivam.mkp@gmail.com>
2020-07-21 19:17:55 +0530

    adding contact

--- src/Component/Contact Us/Contact.js
+++ src/Component/Contact Us/Contact.js
@@ -4,14 +4,9 @@ const Contact = () => {
   return (
     <div className="contact">
       <h1> Contact Us</h1>
-      <div>
-        <h3>North Headquarters </h3>
-        <li>Email: ngocewa@gmail.com</li>
-        <li>Phone.No: +91 733-799-18389</li>
-        <li>
-          Address: F-88, Alpha 2, Greater Noida <br /> Region: Uttar Pradesh
-        </li>
-        <li>Pincode: 210310</li>
+      <div className="main">
+        <div className="main-back"></div>
+        <div className="main-text">Work on contruction</div>
       </div>
     </div>
   );
