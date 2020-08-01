4d53e1b81294954bf02ceb7f73f641f121266c82
shivam1534 <shivam.mkp@gmail.com>
2020-08-01 20:19:09 +0530

    gallery+1

--- src/Component/Gallery/Gallery.js
+++ src/Component/Gallery/Gallery.js
@@ -16,51 +16,50 @@ class Gallery extends React.Component {
           <div className="text"></div>
         </div>
         <div className="top">
+          <h3>Best Of CEWA</h3> <h4>August</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph51"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph61"></div>
+              <div className="photo2 ph62"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph71"></div>
+              <div className="photo3 ph72"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph81"></div>
+              <div className="photo4 ph82"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
           <h3>Lakhimpur Kheri Kit Distribution</h3> <h4>July 2020</h4>
         </div>
         <div className="photoFrame">
           <div className="left">
             <div className="row1">
-              <div className="photo1"></div>
+              <div className="photo1 ph11"></div>
             </div>
             <div className="row2">
-              <div className="photo2"></div>
-              <div className="photo2"></div>
+              <div className="photo2 ph21"></div>
+              <div className="photo2 ph22"></div>
             </div>
           </div>
           <div className="right">
             <div className="row1">
-              <div className="photo3"></div>
-              <div className="photo3"></div>
+              <div className="photo3 ph31"></div>
+              <div className="photo3 ph32"></div>
             </div>
             <div className="row2">
-              <div className="photo4"></div>
-              <div className="photo4"></div>
-            </div>
-          </div>
-        </div>
-
-        <div className="top">
-          <h3>Event Name</h3> <h4>Date of Event</h4>
-        </div>
-        <div className="photoFrame">
-          <div className="left">
-            <div className="row1">
-              <div className="photo1"></div>
-            </div>
-            <div className="row2">
-              <div className="photo2"></div>
-              <div className="photo2"></div>
-            </div>
-          </div>
-          <div className="right">
-            <div className="row1">
-              <div className="photo3"></div>
-              <div className="photo3"></div>
-            </div>
-            <div className="row2">
-              <div className="photo4"></div>
-              <div className="photo4"></div>
+              <div className="photo4 ph41"></div>
+              <div className="photo4 ph42"></div>
             </div>
           </div>
         </div>
