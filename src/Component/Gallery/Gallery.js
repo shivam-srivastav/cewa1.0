4e2f72fd3243f287bfbad84800cad11ca840221b
shivam1534 <shivam.mkp@gmail.com>
2020-09-02 00:26:43 +0530

    gallery updated

--- src/Component/Gallery/Gallery.js
+++ src/Component/Gallery/Gallery.js
@@ -63,6 +63,247 @@ class Gallery extends React.Component {
             </div>
           </div>
         </div>
+        <div className="top">
+          <h3>EVENT 3</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph91"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph101"></div>
+              <div className="photo2 ph102"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph111"></div>
+              <div className="photo3 ph112"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph121"></div>
+              <div className="photo4 ph122"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 4</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph131"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph141"></div>
+              <div className="photo2 ph142"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph151"></div>
+              <div className="photo3 ph152"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph161"></div>
+              <div className="photo4 ph162"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 5</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph171"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph181"></div>
+              <div className="photo2 ph182"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph191"></div>
+              <div className="photo3 ph192"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph201"></div>
+              <div className="photo4 ph202"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 6</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph211"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph221"></div>
+              <div className="photo2 ph222"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph231"></div>
+              <div className="photo3 ph232"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph241"></div>
+              <div className="photo4 ph242"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 7</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph251"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph261"></div>
+              <div className="photo2 ph262"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph271"></div>
+              <div className="photo3 ph272"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph281"></div>
+              <div className="photo4 ph282"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 8</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph291"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph301"></div>
+              <div className="photo2 ph302"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph311"></div>
+              <div className="photo3 ph312"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph321"></div>
+              <div className="photo4 ph322"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 9</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph331"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph341"></div>
+              <div className="photo2 ph342"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph351"></div>
+              <div className="photo3 ph352"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph361"></div>
+              <div className="photo4 ph362"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 10</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph371"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph381"></div>
+              <div className="photo2 ph382"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph391"></div>
+              <div className="photo3 ph392"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph401"></div>
+              <div className="photo4 ph402"></div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="top">
+          <h3>EVENT 6</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph411"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph421"></div>
+              <div className="photo2 ph422"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph431"></div>
+              <div className="photo3 ph432"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph441"></div>
+              <div className="photo4 ph442"></div>
+            </div>
+          </div>
+        </div>
+        <div className="top">
+          <h3>EVENT 6</h3> <h4>July 2020</h4>
+        </div>
+        <div className="photoFrame">
+          <div className="left">
+            <div className="row1">
+              <div className="photo1 ph451"></div>
+            </div>
+            <div className="row2">
+              <div className="photo2 ph461"></div>
+              <div className="photo2 ph462"></div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="row1">
+              <div className="photo3 ph471"></div>
+              <div className="photo3 ph472"></div>
+            </div>
+            <div className="row2">
+              <div className="photo4 ph481"></div>
+              <div className="photo4 ph482"></div>
+            </div>
+          </div>
+        </div>
+       */}
       </div>
     );
   }
